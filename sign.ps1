05849cdb7009b904bfaa1c6f1dbf8275b73a5f76
Sarin Na Wangkanai <sarin@wangkanai.com>
2025-03-29 12:21:36 +0700

    Refactor build scripts and update project configurations
    Simplified dotnet commands in `sign.ps1` for consistency and removed redundant slnx file reference. Marked projects as non-packable where applicable to streamline packing process. These changes improve maintainability and align with project conventions.

--- sign.ps1
+++ sign.ps1
@@ -12,14 +12,14 @@ new-item -Path artifacts -ItemType Directory -Force | out-null
 new-item -Path signed    -ItemType Directory -Force | out-null
 
 dotnet --version
-dotnet clean   .\src\ -c Release -tl
-dotnet restore .\src\
-dotnet build   .\src\ -c Release -tl
+dotnet clean    -c Release -tl
+dotnet restore
+dotnet build    -c Release -tl
 Get-ChildItem  .\src\ -Recurse Wangkanai.*.dll | where { $_.Directory -like "*Release*" } | foreach {
     signtool sign /fd SHA256 /t http://timestamp.digicert.com /n $name $_.FullName
 }
 
-dotnet pack .\src\ -c Release -tl -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
+dotnet pack -c Release -tl -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
 
 dotnet nuget sign .\artifacts\*.nupkg  -v normal --timestamper http://timestamp.digicert.com --certificate-subject-name $name -o .\signed
 dotnet nuget sign .\artifacts\*.snupkg -v normal --timestamper http://timestamp.digicert.com --certificate-subject-name $name -o .\signed
